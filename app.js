fe0f2f12bee3dcf33b288100c9c19ead313d198a
Welcome@1 <adarsh.s@srmtechsol.com>
2025-01-10 16:11:18 +0530

    Working on checnging

--- app.js
+++ app.js
@@ -2,15 +2,24 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const userRoutes = require('./routes/userRoutes');
+const cors=require("cors");
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
 mongoose.connect('mongodb://localhost:27017/userDetailsDB', {
 });
 
+
+app.use(cors());
+
+
 app.use(bodyParser.json());
+// app.use(
+//     cors({origin: ['http://localhost:3000']})
+//    );
 app.use('/api', userRoutes);
+
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
